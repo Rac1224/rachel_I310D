83bba864348073f513b41263b72c8eb1b3e7e119
Rachel Foong <136007905+Rac1224@users.noreply.github.com>
2024-04-12 23:29:38 -0500

    Add files via upload

---
+++ volume_of_sphere.py
@@ -0,0 +1,12 @@
+def calculate_volume_of_sphere(radius):
+		pi = 3.14
+		area = pi * 4 / 3 * radii ** 3
+		return area
+
+radii1 = 30
+area1 = calculate_volume_of_sphere(radii1)
+print(f"The area of sphere with radii {radii1} is: {area1}")
+
+radii2 = 40
+area2 = calculate_volume_of_sphere(radii2)
+print(f"The area of circle with radii {radii2} is: {area2}")
